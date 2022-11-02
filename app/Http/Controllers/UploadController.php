5bed4a42f386484371b36161b6c3834635fe2292
li-linfeng <18013272501@163.com>
2022-11-02 18:16:27 +0800

    修改文件名校验

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -9,10 +9,15 @@ class UploadController extends Controller
 {
     public function upload(Request $request)
     {
+        $filename = $request->file('file')->getClientOriginalName();
+        if(!preg_match('/^[\x{4e00}-\x{9fa5}A-Za-z0-9\-\_\.\(\（\)\）]*$/u', $filename)){
+            abort("422", "文件名仅支持字母,数字,汉字,括号,下划线(_),短划线(-)");
+        }
+
 
         $path = $request->file('file')->storeAs(
             'uploads/' . date("Ymd"),
-            $request->file('file')->getClientOriginalName(),
+             $filename,
             'public'
         );
 
@@ -20,7 +25,7 @@ class UploadController extends Controller
             'type'        => $request->type,
             'source_type' => $request->source_type,
             'path'        => $path,
-            'filename'    => $request->file('file')->getClientOriginalName(),
+            'filename'    => $filename,
         ]);
         $data = $file->toArray();
         $data['name'] = $data['filename'];
